6b461d495cdc792c96c112469071f51f7c0218a2
Roberto Carlos <tj.roberto.olivera.c@upds.net.bo>
2024-04-02 09:34:19 -0400

    Actividad 3

--- GestionEmpresa/GestionEmpresa.VISTA/ClienteVistas/ClienteEditarVista.Designer.cs
+++ GestionEmpresa/GestionEmpresa.VISTA/ClienteVistas/ClienteEditarVista.Designer.cs
@@ -35,11 +35,11 @@
             textBox3 = new TextBox();
             textBox2 = new TextBox();
             textBox1 = new TextBox();
-            label5 = new Label();
-            label4 = new Label();
-            label3 = new Label();
             label2 = new Label();
             label1 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
             SuspendLayout();
             // 
             // button2
@@ -98,37 +98,10 @@
             textBox1.Size = new Size(322, 27);
             textBox1.TabIndex = 47;
             // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(142, 296);
-            label5.Name = "label5";
-            label5.Size = new Size(66, 20);
-            label5.TabIndex = 46;
-            label5.Text = "CORREO";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(186, 236);
-            label4.Name = "label4";
-            label4.Size = new Size(22, 20);
-            label4.TabIndex = 45;
-            label4.Text = "CI";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(128, 176);
-            label3.Name = "label3";
-            label3.Size = new Size(80, 20);
-            label3.TabIndex = 44;
-            label3.Text = "TELEFONO";
-            // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(125, 116);
+            label2.Location = new Point(128, 116);
             label2.Name = "label2";
             label2.Size = new Size(83, 20);
             label2.TabIndex = 43;
@@ -137,17 +110,47 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(138, 56);
+            label1.Location = new Point(141, 57);
             label1.Name = "label1";
             label1.Size = new Size(70, 20);
             label1.TabIndex = 42;
             label1.Text = "NOMBRE";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(126, 293);
+            label6.Name = "label6";
+            label6.Size = new Size(85, 20);
+            label6.TabIndex = 58;
+            label6.Text = "DIRECCION";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(145, 175);
+            label7.Name = "label7";
+            label7.Size = new Size(66, 20);
+            label7.TabIndex = 57;
+            label7.Text = "CORREO";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(131, 234);
+            label8.Name = "label8";
+            label8.Size = new Size(80, 20);
+            label8.TabIndex = 56;
+            label8.Text = "TELEFONO";
             // 
             // ClienteEditarVista
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(758, 471);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label8);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox5);
@@ -155,9 +158,6 @@
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "ClienteEditarVista";
@@ -176,10 +176,10 @@
         private TextBox textBox3;
         private TextBox textBox2;
         private TextBox textBox1;
-        private Label label5;
-        private Label label4;
-        private Label label3;
         private Label label2;
         private Label label1;
+        private Label label6;
+        private Label label7;
+        private Label label8;
     }
 }
